127b33ee8bad6ca5a8c848cf5c7a363de5488917
Trishant <trishantjain4444@gmail.com>
2023-02-17 22:54:04 +0530

    creating blogpost model

--- ShopWithUs/blog/models.py
+++ ShopWithUs/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.TextField(max_length=50, default="")
+    head0 = models.TextField(max_length=50, default="")
+    chead0 = models.TextField(max_length=50, default="")
+    head1 = models.TextField(max_length=50, default="")
+    chead1 = models.TextField(max_length=50, default="")
+    head2 = models.TextField(max_length=50, default="")
+    chead2 = models.TextField(max_length=50, default="")
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='shop/images', default="")
+
+    def __str__(self):
+        return self.title

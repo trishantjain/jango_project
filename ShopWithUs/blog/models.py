09aaa08432a4c529c8696215d70c463dcdf91757
Trishant <trishantjain4444@gmail.com>
2023-02-19 19:45:15 +0530

    changing model's textfield to charfield

--- ShopWithUs/blog/models.py
+++ ShopWithUs/blog/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
-    title = models.TextField(max_length=50, default="")
-    head0 = models.TextField(max_length=50, default="")
-    chead0 = models.TextField(max_length=50, default="")
-    head1 = models.TextField(max_length=50, default="")
-    chead1 = models.TextField(max_length=50, default="")
-    head2 = models.TextField(max_length=50, default="")
-    chead2 = models.TextField(max_length=50, default="")
+    title = models.CharField(max_length=50, default="")
+    head0 = models.CharField(max_length=50, default="")
+    chead0 = models.CharField(max_length=50, default="")
+    head1 = models.CharField(max_length=50, default="")
+    chead1 = models.CharField(max_length=50, default="")
+    head2 = models.CharField(max_length=50, default="")
+    chead2 = models.CharField(max_length=50, default="")
     pub_date = models.DateField()
     thumbnail = models.ImageField(upload_to='shop/images', default="")
 
--- ShopWithUs/shop/models.py
+++ ShopWithUs/shop/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class Product(models.Model):
     product_id = models.AutoField
-    product_name = models.TextField(max_length=30)
-    category = models.TextField(max_length=50, default="")
-    subcategory = models.TextField(max_length=50, default="")
+    product_name = models.CharField(max_length=30)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
-    description = models.TextField(max_length=100)
+    description = models.CharField(max_length=100)
     publish_date = models.DateField()
     image = models.ImageField(upload_to="shop/images",
                               default="", blank=True, null=True)
@@ -20,10 +20,10 @@ class Product(models.Model):
 
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=70)
-    email = models.TextField(max_length=70, default="")
-    phone = models.TextField(max_length=70, default="")
-    desc = models.TextField(max_length=100, default="")
+    name = models.CharField(max_length=70)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=100, default="")
 
     def __str__(self):
         return self.name
@@ -31,20 +31,20 @@ class Contact(models.Model):
 
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
-    item_json = models.TextField(max_length=70, default='')
-    name = models.TextField(max_length=70, default='')
-    email = models.TextField(max_length=100, default='')
-    phone = models.TextField(max_length=70, default='')
-    address = models.TextField(max_length=20, default='')
-    state = models.TextField(max_length=70, default='')
-    city = models.TextField(max_length=70, default='')
-    zip_code = models.TextField(max_length=70, default='')
+    item_json = models.CharField(max_length=70, default='')
+    name = models.CharField(max_length=70, default='')
+    email = models.CharField(max_length=100, default='')
+    phone = models.CharField(max_length=70, default='')
+    address = models.CharField(max_length=20, default='')
+    state = models.CharField(max_length=70, default='')
+    city = models.CharField(max_length=70, default='')
+    zip_code = models.CharField(max_length=70, default='')
 
 
 class OrderUpdate(models.Model):
     update_id = models.AutoField(primary_key=True)
     order_id = models.IntegerField(default='')
-    update_desc = models.TextField(max_length=50, default='')
+    update_desc = models.CharField(max_length=50, default='')
     timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):

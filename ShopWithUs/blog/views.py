0449fbf0b3f59bb96361a2a065425377d2c12ef7
Trishant <trishantjain4444@gmail.com>
2023-02-20 21:53:57 +0530

    added ref to continue reading link

--- ShopWithUs/blog/views.py
+++ ShopWithUs/blog/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Blogpost
 
-def blogContent(request, id):
-    post = Blogpost.objects.filter(post_id = id)[0]
-    return render(request, "blog/blog_content.html", {"post": post})
-
 def blogs(request):
     myPosts = Blogpost.objects.all()
     return render(request, "blog/blogs.html", {'myposts': myPosts})
 
+def blogContent(request, id):
+    post = Blogpost.objects.filter(post_id = id)[0]
+    return render(request, "blog/blog_content.html", {"post": post})
